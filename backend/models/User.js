57cd70c030f1f41b99583717fc8caa639614f4fc
cchay <cchayanin@gmail.com>
2020-11-30 23:00:55 +0700

    add user model

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define(
+		'User',
+		{
+			username: {
+				type: DataTypes.STRING(20),
+				unique: true,
+			},
+			password: {
+				type: DataTypes.STRING,
+			},
+			name: {
+				type: DataTypes.STRING,
+			},
+			is_admin: {
+				type: DataTypes.BOOLEAN,
+			},
+		},
+		{
+			tableName: 'user',
+		},
+	)
+
+	return User
+}
